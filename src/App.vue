995dddbdaf0000e633b305e53c69d2773950851d
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-09-11 10:37:31 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,93 @@
 
 <template>
   <header>
-     header
+    <div class="container">
+      <div>
+         <ul>
+            <li>
+                <img src="/img/dc-logo.png" alt="Logo">
+            </li>
+            <li>
+              <a href="#">
+                <h4>CHARACTERS</h4>
+
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>COMICS</h4>
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>MOVIES</h4>
+              </a>
+             
+            </li>
+            <li>
+              <a href="#">
+                <h4>TV</h4>
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>GAMES</h4>
+
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>COLLECTIBLES</h4>
+
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>VIDEOS</h4>
+
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>FANS</h4>
+
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>NEWS</h4>
+
+              </a>
+              
+
+            </li>
+            <li>
+              <a href="#">
+                <h4>SHOP</h4>
+
+              </a>
+              
+
+            </li>
+         </ul>
+      </div>
+
+    </div>
+     
   </header>
   <main>
     main
@@ -18,5 +104,36 @@
 </template>
 
 <style scoped>
+header {
+  text-align: center;
+  padding: 20px 0;
+} 
+header ul {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+header ul li {
+  list-style: none;
+  margin: 0 5px;
+  padding: 10px;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+  transition: all .2s ease-in-out;
+  
+}
+header ul li {
+  display: flex;
+  list-style: none;
+}
+header ul li a:hover,
+header ul li a.active{
+  color: #0282f9;
+  
+}
 
 </style>
