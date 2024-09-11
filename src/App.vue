d0758f4be9bad00a92aa71641efdc6e57c33b750
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-09-11 18:37:30 +0200

    manca solo il main card

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,7 @@
     <div class="container">
       <div>
         <div>
-
-        </div>
-        <div>
-         <ul>
+          <ul>
             <li>
                 <img src="/img/dc-logo.png" alt="Logo">
             </li>
@@ -90,13 +87,10 @@
               
 
             </li>
-         </ul>
+          </ul>
+        </div>
       </div>
-      </div>
-      
-
-    </div>
-     
+    </div>  
   </header>
   <main>
     <div>
@@ -109,17 +103,54 @@
         </div>
 
       </div>
+      <div class="M-BLUE">
+        <div>
+          <ul>
+            <li>
+              <img class="IMG" src="/img/buy-comics-digital-comics.png" alt="DIGITAL">
+              <a href="#">
+                DIGITAL COMICS
+              </a>
+            </li>
+            <li>
+              <img class="IMG" src="/img/buy-comics-merchandise.png" alt="MERCHANDISING">
+              <a href="#">
+                DC MERCHANDISE
+              </a>
+            </li>
+            <li>
+              <img class="IMG" src="/img/buy-comics-subscriptions.png" alt="SUB">
+              <a href="#">
+                SUBSCRIPTION
+              </a>
+            </li>
+            <li>
+              <img class="IMG" src="/img/buy-comics-shop-locator.png" alt="SHOP">
+              <a href="#">
+                COMIC SHOP LOCATOR
+              </a>
+            </li>
+            <li>
+              <img class="IMG" src="/img/buy-dc-power-visa.svg" alt="POWER">
+              <a href="#">
+                DC POWER VISA
+              </a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
 
     </div>
 
   </main>
   <footer>
     <!--Colonna1-->
-     <div class="F-top">
-      <div>
+      <div class="F-top flex">
+        <div class="PS">
         <ul>
         <li>
-          <h4>DC COMICS</h4>
+          <h4 class="h4">DC COMICS</h4>
         </li>
         <li>
           <a href="#">Characters</a>
@@ -145,7 +176,7 @@
        </ul>
        <ul>
         <li>
-          <h4>SHOP</h4>
+          <h4 class="h4">SHOP</h4>
         </li>
         <li>
           <a href="#">Shop DC</a>
@@ -154,12 +185,13 @@
           <a href="##">Shop DC Collectibles</a>
         </li>
        </ul>
-      </div>
+        </div>
+        
       <!--Colonna2-->
-      <div>
+        <div class="PS">
         <ul>
           <li>
-            <h4>DC</h4>
+            <h4 class="h4">DC</h4>
           </li>
           <li>
             <a href="#">Terms Of Use</a>
@@ -193,24 +225,73 @@
             <a href="###########">Contact Us</a>
           </li>
         </ul>
-      </div>
+        </div>
       <!--Colonna3-->
-      <div>
+        <div class="PS">
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <h4 class="h4">SITES</h4>
+          </li>
+          <li>
+            <a href="#">DC</a>
+          </li>
+          <li>
+            <a href="##">MAD Magazine</a>
+          </li>
+          <li>
+            <a href="###">DC Kids</a>
+          </li>
+          <li>
+            <a href="####">DC Universe</a>
+          </li>
+          <li>
+            <a href="#####">DC Power Visa</a>
+          </li>
         </ul>
-      </div>
-
-     </div>
+        </div>
+        <div class="flex">
+          <img class="POSIT" src="/img/dc-logo-bg.png" alt="DC">
+        </div>
+      </div>  
+     <div class="F-bottom flex">
+        <div>
+          <button class="button">
+            <h4 class="HH">SIGN-UP NOW!</h4>  
+          </button>
+        </div>
+        <div class="flex">
+          <div class="flex">
+            <h4 class="h4B">FOLLOW US</h4>
+          </div>
+          <div class="flex">
+              <div>
+                <a class="af" href="#">
+                    <img src="/img/footer-facebook.png" alt="facebook">  
+                </a>  
+              </div>
+              <div>
+                <a class="af" href="##">
+                    <img src="/img/footer-twitter.png" alt="twitter">
+                </a> 
+              </div>
+              <div>
+                <a class="af" href="###">
+                    <img src="/img/footer-youtube.png" alt="youtube"> 
+                </a>  
+              </div>
+              <div>
+                <a class="af" href="####">
+                    <img src="/img/footer-pinterest.png" alt="pinteest">  
+                </a> 
+              </div>
+              <div>
+                <a class="af2" href="#####">
+                    <img src="/img/footer-periscope.png" alt="periscope">  
+                </a> 
+              </div>
+          </div>
+        </div>
       
-     <div>
-      
-
      </div>
   </footer>
 
@@ -259,8 +340,46 @@ header ul li a.active{
   background-image: url('/img/jumbotron.jpg');
 }
 .M-bottom{
-  height: 600px;
+  height: 200px;
   background-color: black;
+}
+.M-BLUE{
+  background-color: #0282f9;
+}
+.M-BLUE {
+  text-align: center;
+  padding: 20px 0;
+} 
+.M-BLUE ul {
+  list-style: none;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+.M-BLUE ul li {
+  list-style: none;
+  margin: 0 5px;
+  padding: 10px;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+  transition: all .2s ease-in-out;
+  
+}
+.M-BLUE ul li {
+  display: flex;
+  list-style: none;
+}
+.M-BLUE ul li a {
+  text-decoration: none;
+  color: white;
+  margin-top: 40px;
+  margin-left: 10px;
+}
+.IMG{
+  height: 100px;
 }
 footer {
   text-align: center;
@@ -282,12 +401,10 @@ footer ul li {
   text-decoration: none;
   font-weight: bold;
   transition: all .2s ease-in-out;
-  
-}
-footer ul li {
   display: flex;
   list-style: none;
 }
+
 footer ul li a {
   text-decoration: none;
   color: grey;
@@ -298,8 +415,54 @@ footer ul li a.active{
   
 }
 .F-top{
-  height: 420px;
   background-image: url('/img/footer-bg.jpg');
+  background-size: 100%;
+  padding-bottom: 30px;
   
 }
+.F-bottom{
+  background-color: #303030;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+.flex{
+  display: flex;
+}
+.POSIT{
+  margin-left: 350px;
+  margin-top: 30px;
+}
+.af{
+  margin: 10px;
+}
+.af2{
+  margin-right: 100px;
+  margin-left: 10px;
+}
+.h4{
+  padding-top: 15px;
+}
+.h4B{
+  color: #0282f9;
+  margin-top: 10px;
+}
+.PS{
+  padding-left: 100px;
+}
+.H{
+  margin: 10px;
+  padding: 10px;
+}
+.HH{
+  margin: 10px;
+}
+button{
+  color: white;
+  background-color: #303030;
+  border: 2px solid #0282f9;
+  height: 40px;
+  margin-left: 100px;
+}
+
 </style>
